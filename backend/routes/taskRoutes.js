31a7ca744ae48a05cb16efcba521ea76f2e4b528
kashyapanshu3108-ops <kashyapanshu3106@gmail.com>
2026-07-15 14:18:45 +0530

    add get request

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,2 +1,13 @@
 const express = require("express");
 const router = express.Router();
+router.get("/", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      title: "Learn React",
+      completed: false,
+    },
+  ]);
+});
+
+module.exports = router;
